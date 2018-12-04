946be8850bf13030a7d600f2cb5a0166538ac992
Grigoriy Levanov <glevanov@gmail.com>
2018-12-04 20:07:22 +0300

    Fixes linter warnings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  console.log("Welcome to the Brain Games!");
-  console.log("\n");
+  console.log('Welcome to the Brain Games!');
+  console.log('\n');
   const inputName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${inputName}`);
 };
