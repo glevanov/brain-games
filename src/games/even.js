0a0467a1bfba00d0a9f883f36bfb114f27694950
Grigoriy Levanov <glevanov@gmail.com>
2018-12-06 20:03:58 +0300

    Refactors even game as per feedback

--- src/games/even.js
+++ src/games/even.js
@@ -9,29 +9,21 @@ const isEven = num => num % 2 === 0;
 const getNumber = (min = MIN_NUM, max = MAX_NUM) => Math.round(Math.random() * (max - min)) + min;
 
 export default () => {
-  let currentStage = 1;
-  let userFailing = false;
-
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".\n');
-  const inputName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${inputName}\n`);
-  while (currentStage <= MAX_STAGES) {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}\n`);
+  for (let stage = 0; stage < MAX_STAGES; stage += 1) {
     const currentNumber = getNumber();
     const correctAnswer = isEven(currentNumber) ? 'yes' : 'no';
 
     console.log(`Question: ${currentNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
-
     if (userAnswer !== correctAnswer) {
       console.log('Incorrect! Game over');
-      userFailing = true;
-      break;
+      return;
     }
     console.log('Correct!');
-    currentStage += 1;
   }
-  if (!userFailing) {
-    console.log(`Congratulations, ${inputName}!`);
-  }
+  console.log(`Congratulations, ${name}!`);
 };
