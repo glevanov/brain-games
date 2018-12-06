28f8c6200ec3ab9851d083e6477566e1074f6a38
Grigoriy Levanov <glevanov@gmail.com>
2018-12-06 22:22:41 +0300

    Writes calc game

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,7 +1,30 @@
 import gameTemplate from '../gameTemplate';
+import { getNumber } from '../util';
 
-const task = 'What is the result of the expression?';
+const MIN_NUM = 1;
+const MAX_NUM = 50;
+
+const operations = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+const operators = Object.keys(operations);
+const getOperator = () => operators[getNumber(0, operators.length - 1)];
 
 export default () => {
-  gameTemplate(task);
+  const goal = 'What is the result of the expression?';
+  const getTask = () => {
+    const firstNum = getNumber(MIN_NUM, MAX_NUM);
+    const secondNum = getNumber(MIN_NUM, MAX_NUM);
+    const currentOperator = getOperator();
+
+    const currentQuestion = `${firstNum} ${currentOperator} ${secondNum}`;
+    const currentAnswer = `${operations[currentOperator](firstNum, secondNum)}`;
+    return {
+      question: currentQuestion,
+      answer: currentAnswer,
+    };
+  };
+  gameTemplate(goal, getTask);
 };
