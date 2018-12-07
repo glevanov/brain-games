065e9b4583dd92e8415558a49faf52b508537bed
Grigoriy Levanov <glevanov@gmail.com>
2018-12-07 21:41:02 +0300

    Add progression game logic

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,10 +1,31 @@
 import gameTemplate from '../gameTemplate';
 import getNumber from '../utils';
 
+const minNum = 1;
+const maxNum = 30;
+const progressionLength = 10;
+
 const goal = 'What number is missing in the progression?';
 const getTask = () => {
-  const question = null;
-  const answer = null;
+  const getProgression = (num) => {
+    const result = [];
+    for (let i = 1; i <= progressionLength; i += 1) {
+      result.push(num * i);
+    }
+    return result;
+  };
+  const bumpProgression = (arr, num) => arr.map(it => it + num);
+
+  const progression = bumpProgression(
+    getProgression(getNumber(minNum, maxNum)),
+    getNumber(minNum, maxNum),
+  );
+  const answerIndex = Math.floor(progressionLength / 2);
+  const firstHalf = progression.slice(0, answerIndex).join(' ');
+  const secondHalf = progression.slice(answerIndex + 1).join(' ');
+
+  const question = `${firstHalf} .. ${secondHalf}`;
+  const answer = `${progression[answerIndex]}`;
   return {
     question,
     answer,
