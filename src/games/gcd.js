593bf0d03cf7800aa47d84e4b9e977de9d36555d
Grigoriy Levanov <glevanov@gmail.com>
2018-12-07 20:34:58 +0300

    Add GCD game logic via bruteforce

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,9 +1,40 @@
 import gameTemplate from '../gameTemplate';
+import getNumber from '../utils';
+
+const minNum = 1;
+const maxNum = 100;
+
+const getDivisors = (num) => {
+  const result = [];
+  for (let i = num; i > 0; i -= 1) {
+    if (num % i === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+};
+
+const getCommonDivisors = (firstArr, secondArr) => firstArr.filter(
+  it => secondArr.indexOf(it) !== -1,
+);
+
+const getGCD = (firstNum, secondNum) => {
+  if (firstNum === secondNum) {
+    return firstNum;
+  }
+  const firstNumDivisors = getDivisors(firstNum);
+  const secondNumDivisors = getDivisors(secondNum);
+
+  return getCommonDivisors(firstNumDivisors, secondNumDivisors)[0];
+};
 
 const goal = 'Find the greatest common divisor of given numbers.';
 const getTask = () => {
-  const question = null;
-  const answer = null;
+  const firstNum = getNumber(minNum, maxNum);
+  const secondNum = getNumber(minNum, maxNum);
+
+  const question = `${firstNum} ${secondNum}`;
+  const answer = `${getGCD(firstNum, secondNum)}`;
   return {
     question,
     answer,
