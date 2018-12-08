4cfba97269dae24b3a0a2cf25beb31cf14403345
Grigoriy Levanov <glevanov@gmail.com>
2018-12-08 10:34:37 +0300

    Fix game template naming after review

--- src/gameTemplate.js
+++ src/gameTemplate.js
@@ -1,19 +1,19 @@
 import readlineSync from 'readline-sync';
 
-const MAX_STAGES = 3;
+const maxStages = 3;
 
 export default (goal, getTask) => {
   console.log('Welcome to the Brain Games!');
   console.log(`${goal}\n`);
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
-  for (let stage = 0; stage < MAX_STAGES; stage += 1) {
+  for (let stage = 0; stage < maxStages; stage += 1) {
     const task = getTask();
-    const { question, answer: correctAnswer } = task;
+    const { question, answer } = task;
 
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer) {
+    if (userAnswer === answer) {
       console.log('Correct!');
     } else {
       console.log('Incorrect! Game over');
